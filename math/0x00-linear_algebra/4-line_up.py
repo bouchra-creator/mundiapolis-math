f38744ed949e1f227918f2b7f86a244982911bf4
bouchra-creator <78182781+bouchra-creator@users.noreply.github.com>
2021-03-11 22:20:07 +0100

    Add files via upload

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,22 +1,10 @@
 #!/usr/bin/env python3
 
-add_arrays = __import__('4-line_up').add_arrays
-import numpy as np
 
 def add_arrays(arr1, arr2):
-    if(np.shape(arr1) == np.shape(arr2) ):
-        arr= np.add(arr1,arr2)
-        return arr.tolist()
-    else:  
-        return "None"
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
-print(add_arrays(arr1, [1, 2, 3]))
-alexa@ubuntu-xenial:0x00-linear_algebra$ ./4-main.py 
-[6, 8, 10, 12]
-[1, 2, 3, 4]
-[5, 6, 7, 8]
-None
+    if len(arr1) == len(arr2):
+        result = []
+        for i in range(len(arr1)):
+            result.append(arr1[i] + arr2[i])
+        return result
+    return None
