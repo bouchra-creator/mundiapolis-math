3ebd1bc3ab2c069b821104236040a7aad419cf42
bouchra-creator <78182781+bouchra-creator@users.noreply.github.com>
2021-03-19 10:59:34 +0100

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-add_matrices2D = __import__('5-across_the_planes').add_matrices2D
-import numpy as np
+
 def add_matrices2D(mat1, mat2):
     mat1 = np.matrix(mat1)
     mat2 = np.matrix(mat2)
@@ -10,10 +9,3 @@ def add_matrices2D(mat1, mat2):
         return somme.tolist()
     else:  
         return "None"
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
-print(mat1)
-print(mat2)
-print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
