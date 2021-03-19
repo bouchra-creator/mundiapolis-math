e6a6c811d9943a91e29913149aeb2239a2bafa8e
bouchra-creator <78182781+bouchra-creator@users.noreply.github.com>
2021-03-19 15:59:15 +0100

    Update 6-bars.py

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -1,4 +1,4 @@
-#%%
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,6 +15,7 @@ plt.ylabel("Quantity of Fruit")
 for i in range(len(fruit)):
     plt.bar(labels1, fruit[i], bottom=np.sum(fruit[:i], axis=0),
             color=colors[i], label=fruits[i], width=0.5)
+    
 plt.yticks(np.arange(0, 90, 10))
 plt.legend()
 
