3975a5d3851035f64290f6755c0ac1cbcde6b387
bouchra-creator <78182781+bouchra-creator@users.noreply.github.com>
2021-03-19 16:01:43 +0100

    Update 100-gradient.py

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -1,4 +1,4 @@
-#%%
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,6 +8,7 @@ x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 fig = plt.figure()
+
 plt.xlabel('x coordinate (m)')
 plt.ylabel('y coordinate (m)')
 plt.title('Mountain Elevation')
